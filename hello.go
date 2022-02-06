cf95cf313d8d426816c79ffca09ec9aa3a4642da
xiahaohai123 <xiahaohai@qq.com>
2022-02-06 13:39:54 +0800

    语言支持

--- hello.go
+++ hello.go
@@ -5,16 +5,33 @@ import (
 )
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 const englishHelloSuffix = "!"
+const spanish = "Spanish"
+const french = "French"
 
 // Hello hello信息
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name + englishHelloSuffix
+	return greetingPrefix(language) + name + englishHelloSuffix
+}
+
+// 获取前缀信息
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Printf(Hello("world"))
+	fmt.Printf(Hello("world", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -13,14 +13,26 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "English")
 		want := "Hello, Chris!"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("who", "French")
+		want := "Bonjour, who!"
 		assertCorrectMessage(t, got, want)
 	})
 }
